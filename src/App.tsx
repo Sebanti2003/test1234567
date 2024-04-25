4e9524beb48a43947f83937b1b742008380acad2
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-04-25 23:03:31 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <div className=' text-3xl'>Hello world</div>
